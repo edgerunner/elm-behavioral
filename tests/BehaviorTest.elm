f32dd53dec766e84cd2dc60e9dc728ccb41624c8
Mert Torun <mert@merttorun.com>
2020-02-27 21:44:43 +0300

    initial state

---
+++ tests/BehaviorTest.elm
@@ -0,0 +1,10 @@
+module BehaviorTest exposing (..)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "name"
+        [ todo "Make data types" ]
