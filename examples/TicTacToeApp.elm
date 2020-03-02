6946ca0f984a8fb4d52b84179e30166c3c2c72e5
Mert Torun <mert@merttorun.com>
2020-03-02 19:28:59 +0300

    TicTacToe wrapped in a sandbox app

---
+++ examples/TicTacToeApp.elm
@@ -0,0 +1,79 @@
+module TicTacToeApp exposing (main)
+
+import Behavior
+import Browser
+import Html exposing (Html, button, div, li, ol, text)
+import Html.Attributes exposing (reversed)
+import Html.Events exposing (onClick)
+import TicTacToe exposing (..)
+
+
+
+-- PROGRAM
+
+
+main : Program () State GameEvent
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = Behavior.fire
+        }
+
+
+
+-- MODEL
+
+
+type alias State =
+    Behavior.State GameEvent
+
+
+init : State
+init =
+    Behavior.initialize
+        (automatedO ++ initialState)
+
+
+
+-- VIEW
+
+
+view : State -> Html GameEvent
+view state =
+    div []
+        [ div [] <| List.map (cell state) [ TopLeft, Top, TopRight ]
+        , div [] <| List.map (cell state) [ Left, Center, Right ]
+        , div [] <| List.map (cell state) [ BottomLeft, Bottom, BottomRight ]
+        , ol [ reversed True ] <| List.map eventLi (Behavior.log state)
+        ]
+
+
+cell : State -> Cell -> Html GameEvent
+cell state currentCell =
+    let
+        mark =
+            List.foldl playMark "-" (Behavior.log state)
+
+        playMark event str =
+            case event of
+                Play player eventCell ->
+                    case ( eventCell == currentCell, player ) of
+                        ( True, X ) ->
+                            "X"
+
+                        ( True, O ) ->
+                            "O"
+
+                        _ ->
+                            str
+
+                _ ->
+                    str
+    in
+    button [ onClick (Click currentCell) ] [ text mark ]
+
+
+eventLi : GameEvent -> Html GameEvent
+eventLi event =
+    li [] [ text <| Debug.toString event ]
