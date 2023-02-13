c9df308beefe7b968d4c4d0662272fbc9b6e984a
Mert Torun <mert@merttorun.com>
2023-02-13 23:11:18 +0300

    Use the elm-css approach for TicTacToe styles

--- examples/TicTacToeApp.elm
+++ examples/TicTacToeApp.elm
@@ -2,13 +2,11 @@ module TicTacToeApp exposing (main)
 
 import Behavior exposing (State)
 import Behavior.Program
-import Css
+import Css exposing (Style)
 import Css.Global exposing (Snippet)
-import Css.Transitions
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
-import Html.Styled
+import Html.Styled as Html exposing (Html, button, div, text)
+import Html.Styled.Attributes exposing (css)
+import Html.Styled.Events exposing (onClick)
 import TicTacToe exposing (..)
 
 
@@ -19,7 +17,7 @@ import TicTacToe exposing (..)
 main : Program () (State GameEvent) GameEvent
 main =
     Behavior.Program.sandbox
-        { view = view
+        { view = view >> Html.toUnstyled
         , behavior = singlePlayer
         }
 
@@ -33,28 +31,17 @@ view state =
     let
         { topLeft, top, topRight, left, center, right, bottomLeft, bottom, bottomRight } =
             extractGrid state
+
+        row =
+            List.map2 (state |> turn |> cell)
     in
-    div [ state |> turn |> turnClass ]
-        [ Css.Global.global [ style ] |> Html.Styled.toUnstyled
-        , div [] <| List.map2 cell [ TopLeft, Top, TopRight ] [ topLeft, top, topRight ]
-        , div [] <| List.map2 cell [ Left, Center, Right ] [ left, center, right ]
-        , div [] <| List.map2 cell [ BottomLeft, Bottom, BottomRight ] [ bottomLeft, bottom, bottomRight ]
+    div [ css [ gameStyle ] ]
+        [ Css.Global.global [ globalCSS ]
+        , div [ css [ rowStyle ] ] <| row [ TopLeft, Top, TopRight ] [ topLeft, top, topRight ]
+        , div [ css [ rowStyle ] ] <| row [ Left, Center, Right ] [ left, center, right ]
+        , div [ css [ rowStyle ] ] <| row [ BottomLeft, Bottom, BottomRight ] [ bottomLeft, bottom, bottomRight ]
         , state |> endgame |> endgameView
         ]
-
-
-turnClass : Maybe Player -> Html.Attribute e
-turnClass turn =
-    class <|
-        case turn of
-            Nothing ->
-                "gameover"
-
-            Just X ->
-                "turn-x"
-
-            Just O ->
-                "turn-o"
 
 
 endgameView : Maybe GameEvent -> Html GameEvent
@@ -78,7 +65,7 @@ endgameView =
         >> Maybe.map
             (text
                 >> List.singleton
-                >> div [ class "endgame", onClick Restart ]
+                >> div [ css [ endgameStyle ], onClick Restart ]
             )
         >> Maybe.withDefault (text "")
 
@@ -100,120 +87,117 @@ extractGrid state =
     latestBoardUpdate <| Behavior.log state
 
 
-cell : Cell -> Mark -> Html GameEvent
-cell currentCell cellMark =
+cell : Maybe Player -> Cell -> Mark -> Html GameEvent
+cell turn currentCell cellMark =
     let
-        ( markChar, highlightClass ) =
+        ( markChar, markStyles ) =
             case cellMark of
                 Blank ->
-                    ( "", "blank" )
+                    ( case turn of
+                        Just X ->
+                            "X"
+
+                        Just O ->
+                            "O"
+
+                        Nothing ->
+                            ""
+                    , [ cellStyle
+                      , blankCellStyles
+                      , Css.cursor Css.pointer
+                      ]
+                    )
 
                 Marked X ->
-                    ( "X", "plain x" )
+                    ( "X", [ cellStyle ] )
 
                 Marked O ->
-                    ( "O", "plain o" )
+                    ( "O", [ cellStyle ] )
 
                 Highlighted X ->
-                    ( "X", "highlighted x" )
+                    ( "X", [ cellStyle, highlightedCellStyle ] )
 
                 Highlighted O ->
-                    ( "O", "highlighted o" )
+                    ( "O", [ cellStyle, highlightedCellStyle ] )
     in
-    button [ onClick (Click currentCell), class highlightClass ] [ text markChar ]
+    button [ onClick (Click currentCell), css markStyles ] [ text markChar ]
 
 
-style : Snippet
-style =
+gameStyle : Style
+gameStyle =
+    Css.batch
+        [ Css.property "display" "grid"
+        , Css.property "grid-template-columns" "repeat(3,6rem)"
+        , Css.property "grid-template-rows" "repeat(3,6rem)"
+        , Css.padding (Css.em 0.5)
+        , Css.property "gap" "0.5em"
+        , Css.justifyContent Css.center
+        , Css.property "align-content" "center"
+        , Css.backgroundColor palette.coral
+        , Css.height (Css.vh 100)
+        , Css.boxSizing Css.borderBox
+        , Css.position Css.relative
+        ]
+
+
+rowStyle : Style
+rowStyle =
+    Css.batch [ Css.property "display" "contents" ]
+
+
+endgameStyle : Style
+endgameStyle =
+    Css.batch
+        [ Css.position Css.absolute
+        , Css.displayFlex
+        , Css.fontSize (Css.em 2)
+        , Css.textAlign Css.center
+        , Css.property "inset" "0.5em"
+        , Css.property "grid-row" "2/2"
+        , Css.property "grid-column" "1/-1"
+        , Css.margin Css.auto
+        , Css.alignItems Css.center
+        , Css.justifyContent Css.center
+        , Css.backgroundColor palette.bisque
+        , Css.padding2 (Css.rem 0.5) (Css.rem 1)
+        , Css.borderRadius (Css.rem 1)
+        , Css.border3 (Css.rem 0.5) Css.solid palette.coral
+        , Css.cursor Css.pointer
+        ]
+
+
+cellStyle : Style
+cellStyle =
+    Css.batch
+        [ Css.fontSize (Css.rem 3)
+        , Css.borderStyle Css.none
+        , Css.backgroundColor palette.white
+        , Css.color palette.black
+        ]
+
+
+highlightedCellStyle : Style
+highlightedCellStyle =
+    Css.batch
+        [ Css.backgroundColor palette.black
+        , Css.color palette.white
+        ]
+
+
+blankCellStyles : Style
+blankCellStyles =
+    Css.batch
+        [ Css.color palette.bisque
+        , Css.hover
+            [ Css.color palette.coral ]
+        ]
+
+
+globalCSS : Snippet
+globalCSS =
     Css.Global.body
         [ Css.boxSizing Css.borderBox
         , Css.fontFamilies [ "Lato", "Verdana", Css.sansSerif.value ]
-        , Css.Global.children
-            [ Css.Global.div
-                [ Css.property "display" "grid"
-                , Css.property "grid-template-columns" "repeat(3,6rem)"
-                , Css.property "grid-template-rows" "repeat(3,6rem)"
-                , Css.padding (Css.em 0.5)
-                , Css.property "gap" "0.5em"
-                , Css.justifyContent Css.center
-                , Css.property "align-content" "center"
-                , Css.backgroundColor palette.coral
-                , Css.height (Css.vh 100)
-                , Css.boxSizing Css.borderBox
-                , Css.position Css.relative
-                , Css.Global.children
-                    [ Css.Global.div
-                        [ Css.property "display" "contents" ]
-                    , Css.Global.class "endgame"
-                        [ Css.position Css.absolute
-                        , Css.displayFlex
-                        , Css.fontSize (Css.em 2)
-                        , Css.textAlign Css.center
-                        , Css.property "inset" "0.5em"
-                        , Css.property "grid-row" "2/2"
-                        , Css.property "grid-column" "1/-1"
-                        , Css.margin Css.auto
-                        , Css.alignItems Css.center
-                        , Css.justifyContent Css.center
-                        , Css.backgroundColor palette.bisque
-                        , Css.padding2 (Css.rem 0.5) (Css.rem 1)
-                        , Css.borderRadius (Css.rem 1)
-                        , Css.border3 (Css.rem 0.5) Css.solid palette.coral
-                        , Css.cursor Css.pointer
-                        ]
-                    ]
-                , Css.Global.descendants
-                    [ Css.Global.button
-                        [ Css.fontSize (Css.rem 3)
-                        , Css.backgroundColor palette.white
-                        , Css.borderStyle Css.none
-                        , Css.Global.withClass "highlighted"
-                            [ Css.backgroundColor palette.black
-                            , Css.color palette.white
-                            ]
-                        ]
-                    ]
-                , Css.Global.withClass "turn-x"
-                    [ Css.Global.descendants
-                        [ Css.Global.button
-                            [ Css.opacity (Css.num 1)
-                            , Css.Global.withClass "blank"
-                                [ Css.opacity (Css.num 0.4)
-                                , Css.after
-                                    [ Css.property "content" "'X'"
-                                    , Css.opacity (Css.num 0.05)
-                                    ]
-                                , Css.hover
-                                    [ Css.after
-                                        [ Css.opacity (Css.num 0.4) ]
-                                    ]
-                                ]
-                            , Css.Global.withClass "o"
-                                [ Css.Transitions.transition
-                                    [ Css.Transitions.opacity3 100 100 Css.Transitions.easeIn
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                , Css.Global.withClass "turn-o"
-                    [ Css.Global.descendants
-                        [ Css.Global.button
-                            [ Css.Global.withClass "blank"
-                                [ Css.after
-                                    [ Css.property "content" "'O'"
-                                    , Css.opacity (Css.num 0.05)
-                                    ]
-                                , Css.hover
-                                    [ Css.after
-                                        [ Css.opacity (Css.num 0.4) ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
         ]
 
 
